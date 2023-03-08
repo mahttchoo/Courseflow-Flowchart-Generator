a0c8e69ad0cc86b71f058ee4fb97f328e5628e18
mahttchoo <56457050+mahttchoo@users.noreply.github.com>
2023-03-08 12:27:14 -0800

    Got lemon library working, pushing changes to my branch

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <lemon/list_graph.h>
 
+using namespace lemon;
 using namespace std;
 
-int main() {
-    cout << "Hello World!" << endl;
+int main()
+{
+    ListDigraph g;
+    ListDigraph::Node u = g.addNode();
+    ListDigraph::Node v = g.addNode();
+    ListDigraph::Node w = g.addNode();
+    ListDigraph::Arc  a = g.addArc(u, v);
+    cout << "Hello World! This is LEMON library here." << endl;
+    cout << "We have a directed graph with " << countNodes(g) << " nodes "
+         << "and " << countArcs(g) << " arc." << endl;
     return 0;
-};
+}
