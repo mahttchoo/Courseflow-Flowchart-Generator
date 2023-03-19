34700c84e6aa7fc18c6028633a929d8366ba5d8c
mclemmer7 <clemmerm@spu.edu>
2023-03-19 16:37:30 -0700

    Adding the flowchart display

--- graphics.py
+++ graphics.py
@@ -1,24 +1,11 @@
-# Must type in sudo apt install x11-xserver-utils
-# import matplotlib
-# matplotlib.use('Agg')
+from diagrams import Diagram, Cluster, Edge
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
 
-import tkinter as tk
-
-window = tk.Tk()
-window.geometry("800x500")
-#window.attributes("-fullscreen", True)
-window.title("Flowchart Graphics")
-
-label = tk.Label(window, text="Year 1", font=('Comic Sans MS', 23))
-label.pack(padx=200,pady=100)
-
-canvas = tk.Canvas(window, width=700, height=300)
-canvas.pack(pady=20)
-rectangle = canvas.create_rectangle(0, 0, 400, 400, fill='blue')
-
-window.mainloop()
-# export DISPLAY=:0.0
-# export DISPLAY=:0.0
-
-#file = open("output.txt", "r")
-#print(file.read())
+with Diagram("Major Flowchart", show=False, direction="TB"):
+    ELB("lb") >> [EC2("course1"),
+                  EC2("course2"),
+                  EC2("course3"),
+                  EC2("course4"),
+                  EC2("course5")] >> RDS("events")
