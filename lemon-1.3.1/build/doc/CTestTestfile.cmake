b7b9290c14aeca55efe35604bc22d4f64ef6b11d
mclemmer7 <clemmerm@spu.edu>
2023-03-20 01:31:32 -0700

    Pushing to main

--- lemon-1.3.1/build/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fiskm/Development/GitHub/flowchart-the-fellowship/lemon-1.3.1/doc
-# Build directory: /home/fiskm/Development/GitHub/flowchart-the-fellowship/lemon-1.3.1/build/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
